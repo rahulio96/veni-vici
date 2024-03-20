5379e7633d3a6336bb194b9e2f94fe5d3bc9fc1e
rahulio96 <rahulgithub96@gmail.com>
2024-03-19 19:38:16 -0700

    Display random, new cat image from API when clicking button

--- src/components/current/Current.jsx
+++ src/components/current/Current.jsx
@@ -1,20 +1,38 @@
 import './Current.css'
+import { useState } from 'react';
 
 
 function Current() {
-
-    const click = () => {
-      
-    }
-
-    return (
-      <div className='current-container'>
-        <h1> Veni Vici!</h1>
-        <p>Discover cats from your wildest dreams?</p>
-        <button onClick={click}>Discover!</button>
-      </div>
-    )
-  }
   
-  export default Current
+  const [imgURL, setImgURL] = useState('');
+
+  const headers = new Headers({
+    "Content-Type": "application/json",
+    "x-api-key": import.meta.env.VITE_API_KEY
+  });
+
+  var requestOptions = {
+    method: 'GET',
+    headers: headers,
+    redirect: 'follow'
+  };
+
+  const newCat = async () => {
+  const response = await fetch("https://api.thecatapi.com/v1/images/search?size=med&mime_types=jpg&format=json&has_breeds=true&order=RANDOM&page=0&limit=1", requestOptions)
+  const data = await response.json()
+  console.log(data[0].url)
+  setImgURL(data[0].url)
+  }
+
+  return (
+    <div className='current-container'>
+      <h1> Veni Vici!</h1>
+      <p>Discover cats from your wildest dreams?</p>
+      <button onClick={newCat}>Discover!</button>
+      <img src={imgURL} />
+    </div>
+  )
+}
+  
+export default Current
   
