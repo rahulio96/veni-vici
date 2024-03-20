fc972ea720614e51f88aae77eb36aea469960bbe
rahulio96 <rahulgithub96@gmail.com>
2024-03-19 18:52:26 -0700

    Ran npm i and added layout w/ responsive sidebar

---
+++ src/components/ban-list/BanList.jsx
@@ -0,0 +1,14 @@
+import './BanList.css'
+
+function BanList() {
+
+    return (
+      <div className="ban-list-container">
+        <h3>Ban List</h3>
+        <p>Select an attribute in your listing to ban it</p>
+      </div>
+    )
+  }
+  
+  export default BanList
+  
---
+++ src/components/current/Current.jsx
@@ -0,0 +1,15 @@
+import './Current.css'
+
+function Current() {
+
+    return (
+      <div className='current-container'>
+        <h1> Veni Vici!</h1>
+        <p>Discover cats from your wildest dreams?</p>
+        <button>Discover!</button>
+      </div>
+    )
+  }
+  
+  export default Current
+  
