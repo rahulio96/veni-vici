6d19d2ac03c3e1429895097024edf34406543828
rahulio96 <rahulgithub96@gmail.com>
2024-03-20 17:04:59 -0700

    Added ability to exclude based on country

--- src/components/current/Current.jsx
+++ src/components/current/Current.jsx
@@ -31,25 +31,34 @@ function Current() {
   const newCat = async () => {
     let isFound = false
     let currentBreed
+    let currentCountry
     while (!isFound) {
       const response = await fetch(URL, requestOptions)
       const data = await response.json()
       currentBreed = data[0].breeds[0].name
-      if (!excludedList.includes(currentBreed)) {
+      currentCountry = data[0].breeds[0].origin
+
+      if (!excludedList.includes(currentBreed) && !excludedList.includes(currentCountry)) {
         console.log(data[0])
         setImgURL(data[0].url)
         setBreed(currentBreed)
         setWeight(data[0].breeds[0].weight.imperial)
         setAge(data[0].breeds[0].life_span)
-        setCountry(data[0].breeds[0].origin)
+        setCountry(currentCountry)
         isFound = true
       }
     }
   }
 
-  const exclude = () => {
+  const excludeBreed = () => {
     if (!excludedList.includes(breed)) {
       setExcludedList([...excludedList, breed])
+    }
+  }
+
+  const excludeCountry = () => {
+    if (!excludedList.includes(country)) {
+      setExcludedList([...excludedList, country])
     }
   }
 
@@ -60,8 +69,8 @@ function Current() {
       <h1> Veni Vici!</h1>
       <p>Discover cats from your wildest dreams?</p>
       <div className='details-container'>
-        {breed == '' ? <></> : <button onClick={exclude} className='details'>{breed}</button>}
-        {country == '' ? <></> : <button className='details'>{country}</button>}
+        {breed == '' ? <></> : <button onClick={excludeBreed} className='details'>{breed}</button>}
+        {country == '' ? <></> : <button onClick={excludeCountry} className='details'>{country}</button>}
         {age == '' ? <></> : <button className='details-no-click'>{age} years</button>}
         {weight == '' ? <></> : <button className='details-no-click'>{weight} lbs</button>}
       </div>
